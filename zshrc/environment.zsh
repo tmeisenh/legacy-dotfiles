06b09b92e441c9d9f39f5e66dea6874f9d227198
tmeisenh <tmeisenh@44c052b4-4e25-0410-8d3a-e90b648b6ecc>
2013-01-27 23:34:37 +0000

    modified for reasons

---
+++ zshrc/environment.zsh
@@ -0,0 +1,14 @@
+# $Id$
+#
+# Z shell configuration file
+#
+# Author: Travis Meisenheimer <travis@indexoutofbounds.com>
+#
+#********************************************************************
+
+export JAVA_HOME=/usr/local/openjdk7
+#export M2_HOME=
+#export GRADLE_HOME=
+
+export PATH=$PATH:$JAVA_HOME/bin
+
