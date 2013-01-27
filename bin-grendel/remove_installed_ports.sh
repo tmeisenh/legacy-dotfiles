8429ea39b13ac004a2227e48aeca283f9430badc
tmeisenh <tmeisenh@44c052b4-4e25-0410-8d3a-e90b648b6ecc>
2013-01-27 18:23:56 +0000

    added script to remove ports

---
+++ bin-grendel/remove_installed_ports.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+
+# sync pkgdb with ports tree
+pkgdb -F
+
+# delete all packages
+pkg_delete -f -a
+
+# delete ports
+rm -rf /var/db/pkg/*
+
+rm -rf /var/db/ports/*
+
+rm -rf /usr/local/*
+
+rm -rf /usr/ports/distfiles/*
