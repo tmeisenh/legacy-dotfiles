7bc210a6196297431d435cc48622a1fbe12cc6b8
Travis 'All-Father' Meisenheimer <tmeisenh@gmail.com>
2013-03-09 23:14:41 -0600

    more linux fixes

--- .zsh-tbm/linux.zsh
+++ .zsh-tbm/linux.zsh
@@ -12,9 +12,16 @@ alias llsd="ls --color -ld -T 0 -b -h -X *(-/DN)"
 alias lsa="ls --color -ld -h -X -b -T 0 .*"
 alias last_mod="ls --color -Flat -h -b -X -T 0"
 
+bindkey "^[OH" beginning-of-line       ## home
+bindkey "^[OF" end-of-line             ## end
+
 # color man pages, whoa...
 VIM=/usr/share/vim/vim73
 export MANPAGER="col -b | $VIM/macros/less.sh -c 'set ft=man nomod nolist nofoldenable' -"
 
 # Hash common directories
 hash -d log=/var/log
+
+function search_apt() {
+ apt-cache search "${1}"
+}
