7e456d3f186b43be3033d9e1016914d628cf5344
Travis 'All-Father' Meisenheimer <tmeisenh@gmail.com>
2013-08-17 18:03:23 -0500

    install seems to work.  it's ugly but we'll see. -travis

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ install-dotfiles() {
 	echo "Installing ZSH..."
 	echo "Backing up your .zshrc if neccessary..."
 	!(ls ${DEST}/.zshrc > /dev/null 2> /dev/null) || mv ${DEST}/.zshrc ${DEST}/zshrc.bak 
-  ls -s ${TOP}/zsh ${DEST}/zsh
+  ln -s ${TOP}/zsh ${DEST}/zsh
 	ln -s ${TOP}/zsh/zshrc ${DEST}/.zshrc 
   mkdir ${DEST}/.zsh-cache
 
@@ -24,10 +24,10 @@ install-dotfiles() {
 	ln -s ${TOP}/vimrc ${DEST}/.vimrc 
 
   echo "setting up bin directories..."
-	mkdir -p ${DEST}/bin
-	cp -a ${TOP}/bin/* ${DEST}/bin/
-	ln -s ${TOP}/bin-freebsd ${DEST}/bin-freebsd
-	ln -s ${TOP}/bin-darwin ${DEST}/bin-darwin
+	#mkdir -p ${DEST}/bin
+	ln -s ${TOP}/bin/ ${DEST}/
+	ln -s ${TOP}/bin-freebsd ${DEST}/
+	ln -s ${TOP}/bin-darwin ${DEST}/
 
   echo "setting up git..."
 	!(ls ${DEST}/.gitconfig > /dev/null 2> /dev/null) || mv ${DEST}/.gitconfig ${DEST}/gitconfig.bak
@@ -42,13 +42,17 @@ uninstall-dotfiles() {
 	echo "Uninstalling zsh, vim, and everything else from ${DEST}..."
 	$rm_cmd -vf ${DEST}/.zshrc 
 	$rm_cmd -vf ${DEST}/.zlogout 
+	$rm_cmd -vf ${DEST}/zsh
+	$rm_cmd -vf ${DEST}/.zprofile
+	$rm_cmd -vrf ${DEST}/.zsh-cache
+	$rm_cmd -vrf ${DEST}/.zcompdump
+
 	$rm_cmd -vf ${DEST}/.logout 
+
 	$rm_cmd -vf ${DEST}/.vimrc 
+	$rm_cmd -vf ${DEST}/.viminfo 
 	$rm_cmd -vf ${DEST}/vim
 	$rm_cmd -vf ${DEST}/.dir_colors
-	$rm_cmd -vf ${DEST}/zsh
-	$rm_cmd -vrf ${DEST}/.zsh-cache
-	$rm_cmd -vf ${DEST}/.zshrc
 
 	$rm_cmd -vrf ${DEST}/bin
 	$rm_cmd -vrf ${DEST}/bin-freebsd
@@ -56,4 +60,4 @@ uninstall-dotfiles() {
 }
 
 uninstall-dotfiles
-#install-dotfiles
+install-dotfiles
