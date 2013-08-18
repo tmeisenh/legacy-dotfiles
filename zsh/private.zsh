ff0c7d277e44456fd1479beca7d42e96e6f22f46
Travis 'All-Father' Meisenheimer <tmeisenh@gmail.com>
2013-08-17 20:10:45 -0500

    added private.zsh

---
+++ zsh/private.zsh
@@ -0,0 +1,6 @@
+# Z shell configuration file
+#
+# Author: Travis Meisenheimer <travis@indexoutofbounds.com>
+# Local settings NOT stored in git.
+#********************************************************************
+
