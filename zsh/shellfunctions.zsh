0084362fc2c2da3416f74d54caaddbe61ac651d8
Travis 'All-Father' Meisenheimer <tmeisenh@gmail.com>
2013-09-08 15:58:09 -0500

    added dev update

--- zsh/shellfunctions.zsh
+++ zsh/shellfunctions.zsh
@@ -136,6 +136,10 @@ function update_iob.com {
     ssh tmeisenh@dev.indexoutofbounds.com /home/tmeisenh/install_site.sh
 }
 
+function update_deviob.com {
+    ssh tmeisenh@dev.indexoutofbounds.com /home/tmeisenh/dev_site.sh
+}
+
 # Generates a random password using apg.
 function random_password() {
     echo "Here are ten random passwords for you.  Pick one, or run again. \n\n"
