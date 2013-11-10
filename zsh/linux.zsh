3476bb58f191bb944e6091fd3f3a91ba1b0f6912
Travis Meisenheimer <travis@indexoutofbounds.com>
2013-11-09 22:05:37 -0600

    updated maven

--- zsh/linux.zsh
+++ zsh/linux.zsh
@@ -32,8 +32,8 @@ function search_apt() {
 
 export JAVA_HOME=/usr/lib/jvm/java-6-oracle
 export IDEA_JDK=$JAVA_HOME
-export M2_HOME=/usr/share/maven
+export M3_HOME=/usr/local/apache-maven
 export ANT_HOME=/usr/local/share/java/apache-ant
 export GRADLE_HOME=/usr/local/gradle-1.3
 export ANDROID_HOME=~/android-sdk/sdk
-export PATH=$PATH:$JAVA_HOME/bin:$M2_HOME/bin:$ANT_HOME/bin:$GRADLE_HOME/bin:$HOME/.rvm/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools
+export PATH=$PATH:$JAVA_HOME/bin:$M3_HOME/bin:$ANT_HOME/bin:$GRADLE_HOME/bin:$HOME/.rvm/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools
