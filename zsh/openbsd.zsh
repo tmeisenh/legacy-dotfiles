1a8205a014d7a4d81d99b057efeb023712b3afa9
Travis 'All-Father' Meisenheimer <tmeisenh@gmail.com>
2014-04-22 20:10:44 -0500

    Fixed alias

--- zsh/openbsd.zsh
+++ zsh/openbsd.zsh
@@ -10,6 +10,6 @@ export LANG=$LC_ALL
 export PKG_URL=ftp://openbsd.mirrors.pair.com/pub/OpenBSD/5.4/packages/amd64
 export PKG_PATH
 
-alias ls="colorls -C -G -F -h -X -T"  
+alias ls="colorls -C -G -F -h -x -T"  
 
 export TERM=wsvt25
