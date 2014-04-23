590b499f7dcabd89d3d554c8c07d085227cb8ab3
Travis 'All-Father' Meisenheimer <tmeisenh@gmail.com>
2014-04-22 20:07:49 -0500

    adjusted install to work on non-gnu systems.  this will probably always be a cluster...

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 #
 # For FreeBSD systems, you'll need to manually invoke bash to run
-# this script since /bin/bash isn't a valid shell.
-# /usr/local/bin/bash ./install.sh
+# this script since /usr/local/bin/bash isn't a valid shell.
+# /usr/local/usr/local/bin/bash ./install.sh
 
 SRC="$(pwd)"
 DEST=$HOME
@@ -13,34 +13,34 @@ mv_cmd="/bin/mv"
 
 install-dotfiles() {
 	echo "Installing tmeisenh dotfiles..."
-  $cp_cmd -a ${SRC}/zsh ${DEST}/.zsh-tmeisenh-dotfiles
+  $cp_cmd -pLR ${SRC}/zsh ${DEST}/.zsh-tmeisenh-dotfiles
 
-  !(ls $HOME/private.zsh > /dev/null 2> /dev/null) || $mv_cmd  -v $HOME/private.zsh ${DEST}/.zsh-tmeisenh-dotfiles/private.zsh
+  !(ls $HOME/private.zsh > /dev/null 2> /dev/null) || $mv_cmd $HOME/private.zsh ${DEST}/.zsh-tmeisenh-dotfiles/private.zsh
 
 
-	$cp_cmd -a ${SRC}/zsh/zshrc ${DEST}/.zshrc 
+  $cp_cmd -pLR ${SRC}/zsh/zshrc ${DEST}/.zshrc 
   mkdir ${DEST}/.zsh-cache
 
-	echo "setting links to other dot files..."
-	$cp_cmd -a ${SRC}/zlogout ${DEST}/.zlogout 
-	$cp_cmd -a ${SRC}/logout ${DEST}/.logout 
-	$cp_cmd -a ${SRC}/LS_COLORS ${DEST}/.dir_colors
+  echo "setting links to other dot files..."
+  $cp_cmd -pLR ${SRC}/zlogout ${DEST}/.zlogout 
+  $cp_cmd -pLR ${SRC}/logout ${DEST}/.logout 
+  $cp_cmd -pLR ${SRC}/LS_COLORS ${DEST}/.dir_colors
 
-	echo "DONE with install tasks."
+  echo "DONE with install tasks."
 }
   
 
 uninstall-dotfiles() {
-	echo "Uninstalling zsh from ${DEST}..."
+  echo "Uninstalling zsh from ${DEST}..."
   $mv_cmd ${DEST}/.zsh-tmeisenh-dotfiles/private.zsh $HOME/private.zsh
-	$rm_cmd -f ${DEST}/.zshrc 
-	$rm_cmd -f ${DEST}/.zlogout 
-	$rm_cmd -rf ${DEST}/.zsh-tmeisenh-dotfiles
-	$rm_cmd -f ${DEST}/.zprofile
-	$rm_cmd -rf ${DEST}/.zsh-cache
-	$rm_cmd -rf ${DEST}/.zcompdump
+  $rm_cmd -f ${DEST}/.zshrc 
+  $rm_cmd -f ${DEST}/.zlogout 
+  $rm_cmd -rf ${DEST}/.zsh-tmeisenh-dotfiles
+  $rm_cmd -f ${DEST}/.zprofile
+  $rm_cmd -rf ${DEST}/.zsh-cache
+  $rm_cmd -rf ${DEST}/.zcompdump
 
-	$rm_cmd -f ${DEST}/.logout 
+  $rm_cmd -f ${DEST}/.logout 
 
   echo "Manualy remove $HOME/private.zsh if you didn't alter it."
 }
