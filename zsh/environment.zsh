5e6c5058fb61bb02371d9a813f2f6a34a0f9b9dd
Travis B. Meisenheimer <tmeisenh@gmail.com>
2014-10-23 18:23:29 -0500

    added rbenv support instead of rvm

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -4,9 +4,4 @@
 #
 #********************************************************************
 
-# Add rvm/ruby
-if [[ -s "${HOME}/.rvm/scripts/rvm" ]] ; then
-  source "${HOME}/.rvm/scripts/rvm"
-elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
-  source "/usr/local/rvm/scripts/rvm"
-fi
+eval "$(rbenv init -)"
