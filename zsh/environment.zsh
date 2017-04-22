da805045a9fc126e88f576f0052eeeede2bd9cdf
Travis Meisenheimer <travis@indexoutofbounds.com>
2017-04-22 14:41:17 -0500

    Fixed paring issue

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -5,7 +5,7 @@
 #********************************************************************
 
 eval "$(rbenv init -)"
-[[ -d $HOME/.nvm]] || mkdir -p $HOME/.nvm
+[[ -d $HOME/.nvm ]] || mkdir -p $HOME/.nvm
 
 export NVM_DIR="$HOME/.nvm"
 source "/usr/local/opt/nvm/nvm.sh"  
